6ffc9baea08cc548848f8115a5da342421e2faad
iThiefOff <mariusgagiuog@gmail.com>
2021-04-22 09:21:29 +0300

    Bot

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('DarWeeb bot is ready!')
+});
+
+
+
+client.login('ODMzOTQ3MDMyMDA2ODE5ODcx.YH5vsg.vBSoyK_OQZ2SXYRxAS88JsC66Mg')
